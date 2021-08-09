bf7fec3c1998063b3423078c28a7f3b4a5263b94
Anupam Gupta <mapuna_@outlook.com>
2021-08-09 11:17:04 +0530

    added app02. Can display more that one geography together.

--- src/dash/app01.py
+++ src/dash/app01.py
@@ -1,125 +1,25 @@
 import sys
-from cmd2 import style
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-from pkg_resources import add_activation_listener
 import plotly.express as px
 import plotly.graph_objects as go
 from dash.dependencies import Input, Output
 from dash.exceptions import PreventUpdate
-import datetime
-import dateutil
-import re
 
 import pandas as pd
 
-
 sys.path.append('..')
 from consumer.api_consumer import DataLoader
+from style import *
+
 
 external_stylesheets = [dbc.themes.BOOTSTRAP]
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 dl = DataLoader()
 states = dl.get_all_states_with_codes()
 states = [{'label': key.title(), 'value': value} for key, value in states.items()]
-
-
-def add_range_selector(layout, axis_name='xaxis', ranges=None, default=None):    
-    """Add a rangeselector to the layout if it doesn't already have one.
-    
-    :param ranges: which ranges to add, e.g. ['3m', '1y', 'ytd']
-    :param default_range: which range to choose as the default, e.g. '3m'
-    """
-    axis = layout.setdefault(axis_name, dict())
-    axis.setdefault('type', 'date')
-    axis.setdefault('rangeslider', dict())
-    if ranges is None:
-        # Make some nice defaults
-        ranges = ['1m', '6m', 'ytd', '1y', 'all']
-    re_split = re.compile('(\d+)')
-    def range_split(range):
-        split = re.split(re_split, range)
-        assert len(split) == 3
-        return (int(split[1]), split[2])
-    # plotly understands m, but not d or y!
-    step_map = dict(d='day', m='month', y='year')
-    def make_button(range):
-        range = range.lower()
-        if range == 'all':
-            return dict(step='all')
-        elif range == 'ytd':
-            return dict(count=1,
-                label='YTD',
-                step='year',
-                stepmode='todate')
-        else:
-            (count, step) = range_split(range)
-            step = step_map.get(step, step)
-            return dict(count=count,
-                label=range,
-                step=step,
-                stepmode='backward')
-    axis.setdefault('rangeselector', dict(buttons=[make_button(r) for r in ranges]))
-    if default is not None and default != 'all':
-        end_date = datetime.datetime.today()
-        if default.lower() == 'ytd':
-            start_date = datetime.date(end_date.year, 1, 1)
-        else:
-            (count, step) = range_split(default)
-            step = step_map[step] + 's'  # relativedelta needs plurals
-            start_date = (end_date - dateutil.relativedelta.relativedelta(**{step: count}))
-        axis.setdefault('range', [start_date, end_date])
-
-
-colors = {
-    'background': '#e5ecf6',
-    'text': '#194278',
-    'lines': '#f93b2e',
-}
-
-xaxis = dict(
-    rangeselector=dict(
-        buttons=list([
-            dict(count=1,
-                 label="1m",
-                 step="month",
-                 stepmode="backward"),
-            dict(count=3,
-                 label="3m",
-                 step="month",
-                 stepmode="backward"),
-            dict(count=6,
-                 label="6m",
-                 step="month",
-                 stepmode="backward"),
-            dict(count=1,
-                 label="YTD",
-                 step="year",
-                 stepmode="todate"),
-            dict(count=1,
-                 label="1y",
-                 step="year",
-                 stepmode="backward"),
-            dict(step="all")
-        ])
-    ),
-    rangeslider=dict(
-        visible=True
-    ),
-    type="date",
-    autorange=False,
-    range=[datetime.datetime.now().date() - datetime.timedelta(days=30), datetime.datetime.now().date()]
-)
-
-yaxis = {
-    'autorange': True,
-}
-
-plot_margins = dict(
-    l=5, r=5, b=5, t=5, pad=5
-)
 
 tab1_body = html.Div([
     html.Div([
@@ -194,28 +94,6 @@ card = dbc.Card(
         dbc.CardBody(tab1_body, id="card-content",),
     ],
 )
-
-figure_params = {
-    'title_font_family': 'Arial Black',
-    'xaxis': xaxis,
-    'yaxis': yaxis,
-    'title_x': 0.5,
-    'font_color': colors['text'],
-    'margin': plot_margins,
-}
-
-scatter_style = {
-    'mode': 'markers+lines',
-    'marker': {
-        'color': colors['lines'],
-        'size': 5,
-    },
-    'line': {
-        "width": 0.5,
-        "color": colors['lines']
-    },
-    'showlegend': False,
-}
 
 app.layout = dbc.Container([
     html.H1(children="Covid 19: India Analysis Dashboard", 
