8b67c34b90f2cb850b342e9c3d8b83164f59abc8
Anupam Gupta <mapuna_@outlook.com>
2021-09-23 11:06:30 +0530

    Sep 23 data update

--- src/dash/app_revised.py
+++ src/dash/app_revised.py
@@ -91,7 +91,7 @@ app.layout = html.Div([
             dbc.Tab(label='Read Me', tab_id='tab-2'),
         ],
         id="tabs",
-        active_tab="tab-1",
+        active_tab="tab-4",
     ),
     html.Div(
         id="glance-tab",
@@ -124,7 +124,7 @@ app.layout = html.Div([
             html.Footer(
                 dbc.Row([
                     dbc.Col(
-                        html.H5("Data Last Updated: Sep 22, 2021 @ 11:40 AM"), 
+                        html.H5("Data Last Updated: Sep 23, 2021 @ 11:00 AM"), 
                         align='start', width={'size': 4}
                     ),
                     dbc.Col(
