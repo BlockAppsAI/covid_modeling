45f2de2dcbe7a385516dfd143ade612e39385e84
Anupam Gupta <mapuna_@outlook.com>
2021-10-02 11:02:39 +0530

    Oct 02 data update

--- src/dash/app_revised.py
+++ src/dash/app_revised.py
@@ -124,7 +124,7 @@ app.layout = html.Div([
             html.Footer(
                 dbc.Row([
                     dbc.Col(
-                        html.H5("Data Last Updated: Oct 01, 2021 @ 11:20 AM"), 
+                        html.H5("Data Last Updated: Oct 02, 2021 @ 11:00 AM"), 
                         align='start', width={'size': 4}
                     ),
                     dbc.Col(
